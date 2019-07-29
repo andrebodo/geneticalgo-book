e4c76130df9b5e628b6343654eee3d51e973f575
unknown <andre.bodo@gmail.com>
2019-07-29 12:28:47 -0400

    added annealing to engine

--- genetic.py
+++ genetic.py
@@ -3,6 +3,9 @@ import time
 import random
 import statistics
 
+from math import exp
+from bisect import bisect_left
+
 class Benchmark:
 	@staticmethod
 	def run(function):
@@ -24,6 +27,7 @@ class Benchmark:
 class Chromosome:
 	Genes = None
 	Fitness = None
+	Age = 0 # Track how many generations passed since last improvement
 
 	def __init__(self, genes, fitness):
 		self.Genes = genes
@@ -57,22 +61,49 @@ def _mutate_custom(parent, custom_mutate, get_fitness):
 # Generate sucessively better gene squence and send to get_best
 # using yield -> code does not run when function is called! instead it
 # returns a generator object (single use iterable)
-def _get_improvement(new_child, generate_parent):
-	bestParent = generate_parent() # This refers to the value from the function passed as an arguement
+def _get_improvement(new_child, generate_parent, maxAge):
+	parent = bestParent = generate_parent() # This refers to the value from the function passed as an arguement
 	yield bestParent
+	historicalFitnesses = [bestParent.Fitness] # List of fitnesses of the historical best parents
 
 	while True:
 		child = new_child(bestParent) # This refers to the value from the function passed as an arguement
-		if bestParent.Fitness > child.Fitness:
+		if parent.Fitness > child.Fitness:
+			if maxAge is None:
+				continue
+			parent.Age += 1
+			if maxAge > parent.Age:
+				continue
+			# Annealing - If child gene sequence is far away from the current best solution, give gene
+			# high probabily of continuing, otherwise do something else (implementation specific)
+			# get index location of the child.Fitness in the historicalFitness
+			index = bisect_left(historicalFitnesses, child.Fitness, 0, len(historicalFitnesses)) 
+			difference = len(historicalFitnesses) - index # Get proximity of best fitness
+			proportionSimilar = difference / len(historicalFitnesses)
+			if random.random() < exp(-proportionSimilar): # e^difference = scaled difference 0 to 1
+				parent = child # child becomes new parent if chance is high
+				continue
+			parent = bestParent # otherwise replace parent with best parent, reset age to 0 giving time to anneal
+			parent.Age = 0
 			continue
 		if not child.Fitness > bestParent.Fitness:
-			bestParent = child
+			# same fitness
+			child.Age = parent.Age + 1
+			parent = child
 			continue
 		yield child
-		bestParent = child
+		parent = child
+		parent.Age = 0
+		# when find child with fitness better than best parent, replace best parent, and append to historical fitnesses
+		if child.Fitness > bestParent.Fitness:
+			yield child
+			bestParent = child
+			historicalFitnesses.append(child.Fitness)
 
 
-def get_best(get_fitness, targetLen, optimalFitness, geneSet, display, custom_mutate = None, custom_create = None):
+
+def get_best(get_fitness, targetLen, optimalFitness, geneSet, display, 
+			custom_mutate = None, custom_create = None, maxAge = None):
 	random.seed()
 
 	if custom_mutate is None:
@@ -90,7 +121,7 @@ def get_best(get_fitness, targetLen, optimalFitness, geneSet, display, custom_mu
 			genes = custom_create()
 			return Chromosome(genes, get_fitness(genes))
 
-	for improvement in _get_improvement(fnMutate, fnGenerateParent):
+	for improvement in _get_improvement(fnMutate, fnGenerateParent, maxAge):
 		display(improvement)
 		if not optimalFitness > improvement.Fitness:
 			return improvement
